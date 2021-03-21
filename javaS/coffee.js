02499a9bc1a5ec8777c037c0a26338734d58fadc
moetaji <mohmmad.altaji@gmail.com>
2021-03-21 14:42:36 +0200

    add loop

--- javaS/coffee.js
+++ javaS/coffee.js
@@ -2,22 +2,34 @@ var coffee = prompt("hello coffee lover")
 if (coffee == "hello") {
     document.write('<h3>Welcome</h3>');
 }
-var essprso = prompt("are you essprso person?");
-if (essprso == "yes") {
-    document.write('<img src="javaS/pictures/turkish_coffee_pouring.jpg">/');
+var turkish = ""
+var american = ""
+var favCoffee = prompt("what do you prefer turkish coffe or american?")
+while (favCoffee !== "turkish" && favCoffee !== "american")
+    favCoffee = prompt("please chose turkish/american")
+
+var product = ""
+var qountity = ""
+
+if (favCoffee == "turkish") {
+    product = ('<img src="https://cdn.tragate.com/items/assa-turkish-coffee-79554-14636.jpg" width=400px>/');
+}
+else if (favCoffee == "american") {
+    product = ('<img src="https://conticoffee.co.za/wp-content/uploads/2020/05/product-photograph.jpg" width=400px/>')
+}
+var howPackYouWant = prompt("how much u want to buy?")
+for (var i = 0; i < howPackYouWant; i++) {
+    qountity += product;
+    document.write(product);
+}
+var theme = prompt("chose your theme green or gray")
+console.log(theme)
+if (theme == "gray") {
+    document.body.style.backgroundColor = ('gray');
+}
+else if (theme == "green") {
+    document.body.style.backgroundColor = ('green');
 }
 else {
-    alert("sorry for you :D");
-    console.log(essprso)
-}
-var coffeBean =prompt("green,gray")
-console.log(coffeBean)
-if( coffeBean=="gray"){
-    document.body.style.backgroundColor=('gray');
-}
-else if(coffeBean=="green") {
-    document.body.style.backgroundColor=('green');
-}
-else{
-    document.body.style.backgroundColor=('brown');
+    document.body.style.backgroundColor = ('brown');
 }
